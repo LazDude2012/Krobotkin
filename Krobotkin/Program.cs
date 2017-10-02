ff27a23b8b93e9b4fb8f0d3d839007623f95b099
SubcomandantePT <SubcomandantePT@users.noreply.github.com>
2017-10-01 19:26:08 -0700

    changed version number

--- Krobotkin/Program.cs
+++ Krobotkin/Program.cs
@@ -6,7 +6,7 @@ using Discord.Commands;
 
 namespace KrobotkinDiscord {
     class Program {
-        public const string VERSION = "3.2.0";
+        public const string VERSION = "3.2.1";
 
         public static List<ulong> UsersToKickFromBunker = new List<ulong>();
         public static List<DiscordClient> clients = new List<DiscordClient>();
