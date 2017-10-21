702146992f29fee2f05b017df465bafc0fdf9461
gigimoi <gassmann.gavin@gmail.com>
2017-10-21 09:07:47 -0500

    delete messages after an hour.

---
+++ Krobotkin/modules/ImageDeleter.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace KrobotkinDiscord {
+    class ImageDeleter : Module, IDisposable {
+        public List<Timer> Timers = new List<Timer>();
+
+        public void Dispose() {
+            foreach(Timer timer in Timers) {
+                timer.Dispose();
+            }
+        }
+
+        public override async void ParseMessageAsync(Channel channel, Message message) {
+            foreach(var configChannel in Config.INSTANCE.deletePhotoChannels) {
+                if(message.Channel.Id == configChannel.channel_id && message.Server.Id == configChannel.server_id) {
+                    var timer = new Timer();
+                    timer.Interval = 1000 * 60 * 60; // 1 hour
+                    timer.Elapsed += (sender, e) => {
+                        message.Delete();
+                    };
+                    timer.Start();
+                }
+            }
+        }
+    }
+}
