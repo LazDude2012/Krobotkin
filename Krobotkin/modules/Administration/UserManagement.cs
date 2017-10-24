3650719110ea03b3347bf279582eca34c64370d3
SubcomandantePT <SubcomandantePT@users.noreply.github.com>
2017-10-23 19:03:54 -0700

    added lobby and unlobby commands

--- Krobotkin/modules/Administration/UserManagement.cs
+++ Krobotkin/modules/Administration/UserManagement.cs
@@ -2,9 +2,12 @@
 using System.Linq;
 using Discord;
 using Discord.Commands;
+using System.Collections.Generic;
 
 namespace KrobotkinDiscord.Modules.Administration {
     class UserManagement : Module {
+        Dictionary<User, List<Role>> lobbiedUsers = new Dictionary<User, List<Role>>();
+
         public override void InitiateClient(DiscordClient _client) {
             _client.GetService<CommandService>().CreateCommand("kick")
                 .Description("Kicks a user.")
@@ -52,6 +55,79 @@ namespace KrobotkinDiscord.Modules.Administration {
                     }
                 }
             );
+
+            _client.GetService<CommandService>().CreateCommand("lobby")
+                .Alias("timeout")
+                .Description("Removes a user's roles, which can be reassigned with !unlobby.")
+                .Parameter("user")
+                .Do(e => {
+                    // check permissions
+                    if (Config.INSTANCE.GetPermissionLevel(e.User, e.Server) < 2) {
+                        e.Channel.SendMessage("Sorry, you don't have permission to do that.");
+                        return;
+                    }
+
+                    // get user
+                    User user = e.Message.MentionedUsers.FirstOrDefault();
+                    if (user == null) {
+                        e.Channel.SendMessage("Command usage: !lobby @username");
+                        return;
+                    }
+
+                    // compare permission level
+                    if (Config.INSTANCE.GetPermissionLevel(e.User, e.Server) <= Config.INSTANCE.GetPermissionLevel(user, e.Server)) {
+                        e.Channel.SendMessage("Cannot lobby someone with equal/higher permission level.");
+                        return;
+                    }
+
+                    // check if already lobbied
+                    if (lobbiedUsers.ContainsKey(user)) {
+                        e.Channel.SendMessage("User is already lobbied.");
+                        return;
+                    }
+
+                    // lobby user (remove roles)
+                    List<Role> removedRoles = new List<Role>();
+                    foreach (Role r in user.Roles) {
+                        removedRoles.Add(r);
+                    }
+                    user.RemoveRoles(removedRoles.ToArray());
+                    lobbiedUsers[user] = removedRoles;
+                });
+
+            _client.GetService<CommandService>().CreateCommand("unlobby")
+                .Description("Restores a user's roles.")
+                .Parameter("user")
+                .Do(e => {
+                    // check permissions
+                    if (Config.INSTANCE.GetPermissionLevel(e.User, e.Server) < 2) {
+                        e.Channel.SendMessage("Sorry, you don't have permission to do that.");
+                        return;
+                    }
+
+                    // get user
+                    User user = e.Message.MentionedUsers.FirstOrDefault();
+                    if (user == null) {
+                        e.Channel.SendMessage("Command usage: !unlobby @username");
+                        return;
+                    }
+
+                    // compare permission level
+                    if (Config.INSTANCE.GetPermissionLevel(e.User, e.Server) <= Config.INSTANCE.GetPermissionLevel(user, e.Server)) {
+                        e.Channel.SendMessage("Cannot unlobby someone with equal/higher permission level.");
+                        return;
+                    }
+
+                    // check if already lobbied
+                    if (!lobbiedUsers.ContainsKey(user)) {
+                        e.Channel.SendMessage("User is not lobbied.");
+                        return;
+                    }
+
+                    // unlobby user (restore roles)
+                    user.AddRoles(lobbiedUsers[user].ToArray());
+                    lobbiedUsers.Remove(user);
+                });
         }
     }
 }
